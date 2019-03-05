1e9373343502b38ff3c0805df71b7c5006024918
pando85 <pando855@gmail.com>
2019-03-05 09:33:58 +0100

    Add cache

--- alerts_bot/data.py
+++ alerts_bot/data.py
@@ -1,12 +1,14 @@
 import logging
+import os
 import pandas
 import pytz
+import time
 
 from alpha_vantage.timeseries import TimeSeries
 from datetime import datetime
 from typing import Optional
 
-from alerts_bot.config import ALPHAVANTAGE_API_KEY
+from alerts_bot.config import ALPHAVANTAGE_API_KEY, CHECK_PERIOD
 from alerts_bot.types import Alert
 
 
@@ -17,6 +19,10 @@ NASDAQ_TZ = pytz.timezone('America/New_York')
 
 def _is_stock_market_open(current_time: datetime) -> bool:
     return current_time.hour > 9 and current_time.hour < 16 and current_time.weekday() in range(4)
+
+
+def is_cache_Valid(file_path: str) -> bool:
+    return time.time() < os.path.getmtime(file_path) + CHECK_PERIOD
 
 
 def get_rsi(series: pandas.Series, window_length: int) -> pandas.Series:
@@ -39,14 +45,20 @@ def check_alert(alert: Alert) -> Optional[str]:
         log.debug('Stock market is closed')
         log.debug(f'NASDAQ time is: {current_time_nasdaq_tz}')
         return None
-    ts = TimeSeries(key=ALPHAVANTAGE_API_KEY, output_format='pandas')
 
-    data, meta_data = ts.get_intraday(symbol=alert.symbol, interval='1min', outputsize='compact')
-    log.debug(f'Data for symbol {alert.symbol}: {data.tail()}')
-    window_length = 14
-    close = data['4. close']
-    # Get the difference in price from previous step
-    rsi = get_rsi(close, window_length)
+    cache_path = f'/tmp/{alert.symbol}.csv'
+    if is_cache_Valid(cache_path):
+        log.debug(f'Reading cache file: {cache_path}')
+        rsi = pandas.read_csv(cache_path)
+    else:
+        ts = TimeSeries(key=ALPHAVANTAGE_API_KEY, output_format='pandas')
+        data, meta_data = ts.get_intraday(symbol=alert.symbol, interval='1min', outputsize='compact')
+        log.debug(f'Data for symbol {alert.symbol}: {data.tail()}')
+        window_length = 14
+        close = data['4. close']
+        # Get the difference in price from previous step
+        rsi = get_rsi(close, window_length)
+        rsi.to_csv(cache_path)
     log.debug(f'{alert.symbol} rsi: {rsi} -> max: {alert.max_rsi} {alert.min_rsi}')
 
     if rsi[-1] >= alert.max_rsi:
